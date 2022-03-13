90a2e2b6de236e9677ad305f2700b248377c8bf8
Adiv <asifadiv@gmail.com>
2022-03-13 17:44:31 +0000

    Initial commit

---
+++ API/Exception/ApiException.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorTest.API.Exception;
+
+using System;
+
+public class ApiException : Exception
+{
+    public ApiException(string message, int status, object test, object test1, object test2)
+    {
+    }
+}
+
+public class ApiException<TResult> : ApiException
+{
+    public ApiException(string message, int status, object test, object test1, object test2) : base(message, status,
+        test, test1, test2)
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+using BlazorTest.Data;
+using Test.API;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddHttpClient();
+builder.Services.AddTransient<IIGDBClient, IGDBClient>();
+builder.Services.AddTransient<IItemsClient, ItemsClient>();
+builder.Services.AddTransient<IFirebaseClient, FirebaseClient>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
